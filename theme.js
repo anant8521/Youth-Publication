5ab656e373fcab8beae27741b59e8277e5ebbeed
Anant Kiranmauli <anant852132@gmail.com>
2026-06-03 10:17:10 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -22,10 +22,10 @@ document.body.classList.toggle("light");
 
 if(document.body.classList.contains("light")){
 localStorage.setItem("theme","light");
-btn.innerHTML="☀️";
+btn.innerHTML="🌙";
 }else{
 localStorage.setItem("theme","dark");
-btn.innerHTML="🌙";
+btn.innerHTML="☀️";
 }
 
 });
